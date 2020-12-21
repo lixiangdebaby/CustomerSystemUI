94fb3b00690f8bc74da80a6fb5bbce21a57d77d7
lixiangdebaby <lixiangdebaby@163.com>
2020-12-21 14:55:34 +0800

    晚上下拉效果.

--- app/src/main/java/com/saicmotor/systemui/NaviPanelUI.java
+++ app/src/main/java/com/saicmotor/systemui/NaviPanelUI.java
@@ -4,25 +4,31 @@ import android.content.Context;
 import android.graphics.PixelFormat;
 import android.os.Build;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
-import com.saicmotor.systemui.service.SaicSystemUIService;
+import com.saicmotor.systemui.slidinguppanel.SlidingUpPanelLayout;
 
 import static android.content.Context.WINDOW_SERVICE;
 
 public class NaviPanelUI {
     private WindowManager mWindowManage;
     private WindowManager.LayoutParams mLayoutParams;
-    private View displayView;
+    private SlidingUpPanelLayout mDisplayView;
     private ImageButton mHomeBtn;
     public static boolean mIsStarted = false;
     private Context mContext = SystemUIApplication.getInstance();
     private static NaviPanelUI mNaviPanelUI;
+    private static final String TAG = NaviPanelUI.class.getSimpleName();
+    private ViewGroup.LayoutParams mDispalyViewLayoutParams;
+    private LinearLayout mLinear_sliddown;
 
     public static NaviPanelUI getInstance(Context context){
         if(mNaviPanelUI == null){
@@ -51,16 +57,24 @@ public class NaviPanelUI {
         mLayoutParams.flags |= WindowManager.LayoutParams.FLAG_FULLSCREEN;
         mLayoutParams.flags |= WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS;
         mLayoutParams.width = WindowManager.LayoutParams.MATCH_PARENT;
-        mLayoutParams.height = 800;
+        mLayoutParams.height = 60;
+
         mLayoutParams.x = 0;
         mLayoutParams.y = 0;
     }
     private void showFloatingWindow() {
         if(Settings.canDrawOverlays(mContext)){
             LayoutInflater layoutInflater =LayoutInflater.from(mContext);
-            displayView = layoutInflater.inflate(R.layout.activity_main_test2,null);
-            displayView.setOnTouchListener(new NaviPanelUI.FloatingOnTouchListener());
-            mWindowManage.addView(displayView,mLayoutParams);
+            mDisplayView = (SlidingUpPanelLayout)layoutInflater.inflate(R.layout.panel_view_layout,null);
+            Log.d(TAG,"mIsExpand = "+getPanelState());
+            mDispalyViewLayoutParams = mDisplayView.getLayoutParams();
+            mDisplayView.setOnTouchListener(new NaviPanelUI.FloatingOnTouchListener());
+            mWindowManage.addView(mDisplayView,mLayoutParams);
+            mLinear_sliddown = (LinearLayout)mDisplayView.findViewById(R.id.linear_sliddown);
+            if(mLinear_sliddown != null){
+                mLinear_sliddown.setOnTouchListener(new PanelDownOnTouchListener());
+            }
+            mDisplayView.addPanelSlideListener(new DisplayViewPanelStateLister());
         }
     }
     private class FloatingOnTouchListener implements View.OnTouchListener {
@@ -73,6 +87,7 @@ public class NaviPanelUI {
                 case MotionEvent.ACTION_DOWN:
                     x = (int) event.getRawX();
                     y = (int) event.getRawY();
+                    Log.d(TAG,"x = "+x+"  y = "+y);
                     break;
                 case MotionEvent.ACTION_MOVE:
                    /* int nowX = (int) event.getRawX();
@@ -90,6 +105,48 @@ public class NaviPanelUI {
                     break;
             }
             return false;
+        }
+    }
+    class  PanelDownOnTouchListener implements View.OnTouchListener{
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            Log.d(TAG,"getPanelState() = "+getPanelState()+ " motionEvent.event = "+motionEvent.getAction());
+            if(motionEvent.getAction() == MotionEvent.ACTION_DOWN){
+
+                if(getPanelState() == SlidingUpPanelLayout.PanelState.COLLAPSED){
+                    setNaviPanelUICollaps(true);
+                }
+            }else if(motionEvent.getAction() == MotionEvent.ACTION_UP){
+                if(getPanelState() == SlidingUpPanelLayout.PanelState.EXPANDED){
+                    setNaviPanelUICollaps(false);
+                }
+            }
+            return false;
+        }
+    }
+    public void setNaviPanelUICollaps(boolean isExpand){
+        Log.d(TAG,"isExpand() = "+isExpand);
+        if(isExpand){
+            mLayoutParams.height = 750;
+            mWindowManage.updateViewLayout(mDisplayView, mLayoutParams);
+        }else{
+            mLayoutParams.height = 60;
+            mWindowManage.updateViewLayout(mDisplayView, mLayoutParams);
+        }
+    }
+    public SlidingUpPanelLayout.PanelState getPanelState(){
+        return mDisplayView.getPanelState();
+    }
+    class DisplayViewPanelStateLister implements SlidingUpPanelLayout.PanelSlideListener{
+        @Override
+        public void onPanelSlide(View panel, float slideOffset) {
+
+        }
+        @Override
+        public void onPanelStateChanged(View panel, SlidingUpPanelLayout.PanelState previousState, SlidingUpPanelLayout.PanelState newState) {
+            if(newState == SlidingUpPanelLayout.PanelState.COLLAPSED){
+                setNaviPanelUICollaps(false);
+            }
         }
     }
 }
